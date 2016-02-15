23416f3e89854bfd60c4f872a9b48831b3196169
sergii4 <getman.sergei@gmail.com>
2016-02-15 16:20:56 +0200

    dummy message

---
+++ src/main/java/com/getman/grader/App.java
@@ -0,0 +1,13 @@
+package com.getman.grader;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
